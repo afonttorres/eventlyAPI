b47685f1393ddd26961d17ddde427073dfdc83a5
Agnès Font Torres <afonttorres@gmail.com>
2022-08-30 10:12:14 +0200

    init commit

---
+++ src/main/java/com/example/evently/EventlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.evently;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventlyApplication.class, args);
+	}
+
+}
