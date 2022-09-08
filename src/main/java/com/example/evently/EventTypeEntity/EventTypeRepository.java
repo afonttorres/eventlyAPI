9194baec13411a551b62193c9fafdd8ba17ee5d3
Agnès Font Torres <afonttorres@gmail.com>
2022-09-08 14:01:05 +0200

    cleaned up, EventType folder deleted

--- src/main/java/com/example/evently/EventTypeEntity/EventTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.evently.EventTypeEntity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventTypeRepository extends JpaRepository<EventType, Integer> {
-}
